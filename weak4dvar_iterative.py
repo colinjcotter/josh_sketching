5f8b056bcee807cf0336d28b4eec25bbfbc3cc48
Colin Cotter <colin.cotter@imperial.ac.uk>
2024-02-13 13:46:24 +0000

    preconditioning with no data

--- weak4dvar_iterative.py
+++ weak4dvar_iterative.py
@@ -17,8 +17,10 @@ VP = FunctionSpace(mesh, Pelt)
 W = VU * VP # the mixed space for the coupled x-p system
 
 # solution!
-w = Function(W)
-u, p = split(w)
+wkp1 = Function(W)
+ukp1, pkp1 = split(wkp1)
+wk = Function(W)
+uk, pk = split(wk)
 
 # model parameters
 # initial condition from before seeing the data
@@ -35,33 +37,53 @@ Sigma = Constant(0.1)
 
 # the functional F
 
-# penalty term for initial condition (f0)
-F = 0.5*(u-u0)*(u-u0)/gamma*ds_b # integral over t=0 "surface"
-
 # "data" (g0)
-F += 0.5*(u-u1)*(u-u1)/C*ds_t # integral over t=1 "surface"
+F = 0.5*(ukp1-u1)*(ukp1-u1)/C*ds_t # integral over t=1 "surface"
 
 # "cost"
-F += -0.5*p*p/Sigma*dx
+F += -0.5*pkp1*pkp1/Sigma*dx
 
 # "dynamical constraint"
-F += (p*u.dx(1) + p.dx(0)*u.dx(0))*dx
+F += (pkp1*ukp1.dx(1) + pkp1.dx(0)*ukp1.dx(0))*dx
+# penalty term for initial condition (f0)
+F += 0.5*(ukp1-u0)*(ukp1-u0)/gamma*ds_b # integral over t=0 "surface"
 
 # the equation to solve
-dF = derivative(F, w)
+dF = derivative(F, wkp1)
+
+# preconditioning operator
+# "data" (g0)
+Fp = 0.5*(ukp1-u1)*(ukp1-u1)/C*ds_t # integral over t=1 "surface"
+# "cost"
+Fp += -0.5*pkp1*pkp1/Sigma*dx
+# "dynamical constraint"
+Fp += (pkp1*ukp1.dx(1) + pkp1.dx(0)*ukp1.dx(0))*dx
+
+JFp = derivative(derivative(Fp, wkp1), wkp1)
 
 # make a problem to solve
 daparams = {
-    "ksp_type": "preonly",
+    #"snes_monitor": None,
+    "ksp_type": "gmres",
+    "ksp_monitor": None,
     "pc_type": "lu",
     "pc_factor_mat_solver_type": "mumps"
 }
 
-DAProblem = NonlinearVariationalProblem(dF, w)
+bcs = [DirichletBC(W.sub(0), u0 - gamma*pk, "bottom")]
+
+DAProblem = NonlinearVariationalProblem(dF, wkp1, Jp=JFp, bcs=bcs)
 DASolver = NonlinearVariationalSolver(DAProblem,
                                       solver_parameters=daparams)
-DASolver.solve()
 
-uout, pout = w.subfunctions
+err = 1.0e50
+tol = 1.0e-6
+while err > tol:
+    DASolver.solve()
+    err = errornorm(wk, wkp1)
+    print(err)
+    wk.assign(wkp1)
+    
+uout, pout = wkp1.subfunctions
 f0 = File("test4dvar.pvd")
 f0.write(uout, pout)
