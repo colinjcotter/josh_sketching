91def38fc359b6d6f6ca730284db9f47d32d7c37
Colin Cotter <colin.cotter@imperial.ac.uk>
2024-02-26 13:42:56 +0000

    added boilerplate for josh

--- weak4dvar_iterative.py
+++ weak4dvar_iterative.py
@@ -1,4 +1,5 @@
 from firedrake import *
+from scipy import fft
 
 ncells = 50
 base_mesh = UnitIntervalMesh(ncells)
@@ -65,6 +66,8 @@ bcs = [DirichletBC(W.sub(0), u0 - gamma*pk, "bottom")]
 
 class DiagPC(PCBase):
     def initialize(self, pc):
+        # some standard boilerplate stuff
+        #################################
         if pc.getType() != "python":
             raise ValueError("Expecting PC type python")
         prefix = pc.getOptionsPrefix() + "Diag_"
@@ -79,10 +82,21 @@ class DiagPC(PCBase):
         u, v = context.a.arguments()
         if u.function_space() != v.function_space():
             raise ValueError("Pressure space test and trial space differ")
-
+        ##################################
+        
         V = u.function_space()
         self.xfstar = Cofunction(V.dual())
         self.yf = Function(V)
+
+        self.psol = Function(V)
+        self.usol = Function(V)
+        
+        u_solvers = []
+        p_solvers = []
+        for i in range(nlayers):
+            # make solver for p part, solve into self.psol
+            # make solver for u part, solve into self.usol
+            pass
         
     def update(self, pc):
         pass
@@ -92,9 +106,39 @@ class DiagPC(PCBase):
         # copy petsc vec into Function
         with self.xfstar.dat.vec_wo as v:
             x.copy(v)
-            
-        # THIS IS WHERE WE DO SOMETHING
-            
+
+        # get arrays from self.xfstar for ru and rp (right hand side pieces)
+        ru = self.xfstar.dat.data[0][:].copy()
+        rp = self.xfstar.dat.data[1][:].copy()
+        ru = ru.reshape((ncells+1, nlayers+1))
+        ru = ru[:, 1:] #  remove the initial condition variables
+        rp = rp.reshape((ncells+1, nlayers))
+
+        # rescale and FFT
+
+        ru *= Gamma_u
+        ru = fft.fft(ru, axis=1)
+
+        rp *= Gamma_p
+        rp = fft.fft(rp, axis=1)
+
+        yu = self.yf.dat.data[0][:].copy()
+        yp = self.yf.dat.data[1][:].copy()
+        yu = yu.reshape((ncells+1, nlayers+1))
+        yp = yp.reshape((ncells+1, nlayers))
+
+        for i in range(nlayers):
+            p_solvers[i].solve()
+            u_solvers[i].solve()
+            yp[:, i] = self.psol.dat.data[:]
+            yu[:, i+1] = self.usol.dat.data[:]
+
+        # rescale and IFFT
+        yp = fft.ifft(yp, axis=1)
+        yu = fft.ifft(yu, axis=1)
+        self.yf.dat.data[0][:] = yu
+        self.yf.dat.data[1][:] = yp
+
         # copy petsc vec into Function
         with self.yf.dat.vec_ro as v:
             v.copy(y)
@@ -104,6 +148,17 @@ class DiagPC(PCBase):
 
 # make a problem to solve
 lu_params = {
+    "snes_type": "ksponly",
+    "snes_monitor": None,
+    "ksp_type": "gmres",
+    "ksp_atol": 1.0e-50,
+    "ksp_rtol": 1.0e-8,
+    "ksp_monitor": None,
+    "pc_type": "lu",
+    "pc_factor_mat_solver_type": "mumps"
+}
+
+fft_params = {
     "snes_type": "ksponly",
     "snes_monitor": None,
     "ksp_type": "gmres",
